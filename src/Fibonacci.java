6e2b5ad7db01ca78e600d9107b5fbaad5d08e98c
SubhasmitaBehera <beherasubhasmita1999@gmail.com>
2020-10-03 22:13:27 +0530

    Fibonacci

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,31 +1,31 @@
-//import java.util.Scanner;
-//
-//public class Fibonacci {
-//    public static void main(String[] args) {
-//        int sum=0;
-//        int x=0;
-//        int y=1;
-//        Scanner sc= new Scanner(System.in);
-//        System.out.println("Enter a range to see fibonacci series:- ");
-//        int num= sc.nextInt();
-//        int temp= num;
-//        System.out.println("Fibonacci series of first " +num +" number is :-");
-//        if (num==1){
-//            System.out.println("x");
-//        }
-//        else if(num!=1){
-//            System.out.println(x);
-//            System.out.println(y);
-//            while (num-2 >0){
-//                int z = x+y;
-//                x=y;
-//                y=z;
-//                num--;
-//                System.out.println(z);
-//                sum=sum+z;
-//
-//            }
-//        }
-//        System.out.println("The sum of first "+ temp +" number is :- "+sum);
-//    }
-//}
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int sum=0;
+        int x=0;
+        int y=1;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a range to see fibonacci series:- ");
+        int num= sc.nextInt();
+        int temp= num;
+        System.out.println("Fibonacci series of first " +num +" number is :-");
+        if (num==1){
+            System.out.println("x");
+        }
+        else if(num!=1){
+            System.out.println(x);
+            System.out.println(y);
+            while (num-2 >0){
+                int z = x+y;
+                x=y;
+                y=z;
+                num--;
+                System.out.println(z);
+                sum=sum+z;
+
+            }
+        }
+        System.out.println("The sum of first "+ temp +" number is :- "+sum);
+    }
+}
