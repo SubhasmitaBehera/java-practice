d9716cd27c1822d7b3dc384f6e781b1201190bf7
SubhasmitaBehera <beherasubhasmita1999@gmail.com>
2020-10-03 22:11:06 +0530

    GCD

--- src/GCD.java
+++ src/GCD.java
@@ -1,24 +1,24 @@
-//import java.util.Scanner;
-//
-//public class GCD {
-//    public static void main(String args[]){
-//
-//        System.out.print("Enter two numbers to find GCD :- ");
-//        Scanner Obj = new Scanner(System.in);
-//        int first_num = Obj.nextInt();
-//        int second_num = Obj.nextInt();
-//        int gcd = findGCD(first_num,second_num);
-//        System.out.println("GCD =" + gcd);
-//    }
-//    public static int findGCD(int first_num,int second_num){
-//        while (second_num !=0){
-//            if(first_num > second_num){
-//                first_num = first_num - second_num;
-//            }
-//            else {
-//                second_num = second_num - first_num ;
-//            }
-//        }
-//        return first_num;
-//    }
-//}
+import java.util.Scanner;
+
+public class GCD {
+    public static void main(String args[]){
+
+        System.out.print("Enter two numbers to find GCD :- ");
+        Scanner Obj = new Scanner(System.in);
+        int first_num = Obj.nextInt();
+        int second_num = Obj.nextInt();
+        int gcd = findGCD(first_num,second_num);
+        System.out.println("GCD =" + gcd);
+    }
+    public static int findGCD(int first_num,int second_num){
+        while (second_num !=0){
+            if(first_num > second_num){
+                first_num = first_num - second_num;
+            }
+            else {
+                second_num = second_num - first_num ;
+            }
+        }
+        return first_num;
+    }
+}
