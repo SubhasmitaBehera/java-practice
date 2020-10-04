cd9c12f47fa2c967487550b2e81c71d584f215e7
SubhasmitaBehera <beherasubhasmita1999@gmail.com>
2020-10-04 19:59:54 +0530

    Interface

--- src/interface_class/InterFace_Class2.java
+++ src/interface_class/InterFace_Class2.java
@@ -1,28 +1,28 @@
-//package interface_class;
-//
-//import java.util.Scanner;
-//
-//interface Inter{
-//    void PrintIt(int x);
-//    void Square(int X);
-//}
-//
-//class Child implements Inter {
-//    public void PrintIt(int x){
-//        System.out.println("The number is :- "+x);
-//    }
-//    public void Square(int x){
-//        System.out.println("The square of the number is:- " +x*x);
-//    }
-//}
-//
-//public class InterFace_Class2 {
-//    public static void main(String[] args) {
-//        Child obj = new Child();
-//        Scanner sc = new Scanner(System.in );
-//        System.out.print("Enter a number:- ");
-//        int input= sc.nextInt();
-//        obj.PrintIt(input);
-//        obj.Square(input);
-//    }
-//}
+package interface_class;
+
+import java.util.Scanner;
+
+interface Inter{
+    void PrintIt(int x);
+    void Square(int X);
+}
+
+class Child implements Inter {
+    public void PrintIt(int x){
+        System.out.println("The number is :- "+x);
+    }
+    public void Square(int x){
+        System.out.println("The square of the number is:- " +x*x);
+    }
+}
+
+public class InterFace_Class2 {
+    public static void main(String[] args) {
+        Child obj = new Child();
+        Scanner sc = new Scanner(System.in );
+        System.out.print("Enter a number:- ");
+        int input= sc.nextInt();
+        obj.PrintIt(input);
+        obj.Square(input);
+    }
+}
--- src/interface_class/Interface_Class1.java
+++ src/interface_class/Interface_Class1.java
@@ -1,42 +1,42 @@
-//package interface_class;
-//
-//interface Myclass{
-//    static void calculate(double x){
-//        System.out.println("Hi");
-//
-//    }
-//    void calculate1(double x);
-//
-//}
-//
-//class child1 implements Myclass {
-//    public void calculate1(double x){
-//        System.out.println("Square is :- "+x*x);
-//    }
-//    void calculate(double x){
-//        System.out.println(x);
-//    }
-//
-//}
-//
-//class child2 implements Myclass {
-//    void calculate(double x){
-//        System.out.println(x);
-//    }
-//    public void calculate1(double x){
-//        System.out.println("Cube is :- "+x*x*x);
-//    }
-//}
-//
-//public class Interface_Class1 {
-//    public static void main(String[] args) {
-//        child1 ob1 = new child1();
-//        child2 ob2 = new child2();
-//        ob1.calculate(11);
-//        ob1.calculate1(11);
-//        ob2.calculate(10);
-//        ob2.calculate1(10);
-//        Myclass.calculate(5);
-//
-//    }
-//}
+package interface_class;
+
+interface Myclass{
+    static void calculate(double x){
+        System.out.println("Hi");
+
+    }
+    void calculate1(double x);
+
+}
+
+class child1 implements Myclass {
+    public void calculate1(double x){
+        System.out.println("Square is :- "+x*x);
+    }
+    void calculate(double x){
+        System.out.println(x);
+    }
+
+}
+
+class child2 implements Myclass {
+    void calculate(double x){
+        System.out.println(x);
+    }
+    public void calculate1(double x){
+        System.out.println("Cube is :- "+x*x*x);
+    }
+}
+
+public class Interface_Class1 {
+    public static void main(String[] args) {
+        child1 ob1 = new child1();
+        child2 ob2 = new child2();
+        ob1.calculate(11);
+        ob1.calculate1(11);
+        ob2.calculate(10);
+        ob2.calculate1(10);
+        Myclass.calculate(5);
+
+    }
+}
