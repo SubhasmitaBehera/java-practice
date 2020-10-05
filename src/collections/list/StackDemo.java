d54bd0349a3f659154304e069ed94acc06f9ee8b
SubhasmitaBehera <beherasubhasmita1999@gmail.com>
2020-10-05 20:17:37 +0530

    List

--- src/collections/list/StackDemo.java
+++ src/collections/list/StackDemo.java
@@ -1,27 +1,27 @@
-//package collections.list;
-//import java.util.Stack;
-//
-//public class StackDemo {
-//    public static void main(String[] args) {
-//        Stack<Integer> stack = new Stack<>();
-//        System.out.println("Is the stack empty ? " +stack.empty());
-//        stack.push(11);
-//        stack.push(12);
-//        stack.push(13);
-//        stack.push(14);
-//        stack.push(15);
-//        System.out.println("Is the stack empty ? " +stack.empty());
-//        System.out.println("The elements in the stack is : " +stack);
-//
-//        System.out.println(stack.peek());
-//        System.out.println("The elements in the stack is : " +stack);
-//
-//        System.out.println(stack.pop());
-//        System.out.println(stack.pop());
-//        System.out.println("The elements in the stack is : " +stack);
-//
-//        System.out.println("The position number no. of element is :- " +stack.search(13));
-//        System.out.println("The index number no. of element is :- " +stack.indexOf(13));
-//
-//    }
-//}
+package collections.list;
+import java.util.Stack;
+
+public class StackDemo {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        System.out.println("Is the stack empty ? " +stack.empty());
+        stack.push(11);
+        stack.push(12);
+        stack.push(13);
+        stack.push(14);
+        stack.push(15);
+        System.out.println("Is the stack empty ? " +stack.empty());
+        System.out.println("The elements in the stack is : " +stack);
+
+        System.out.println(stack.peek());
+        System.out.println("The elements in the stack is : " +stack);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println("The elements in the stack is : " +stack);
+
+        System.out.println("The position number no. of element is :- " +stack.search(13));
+        System.out.println("The index number no. of element is :- " +stack.indexOf(13));
+
+    }
+}
