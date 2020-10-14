a13f705566aef22cf5196c5b40786d13c96ad39a
SubhasmitaBehera <beherasubhasmita1999@gmail.com>
2020-10-14 19:46:19 +0530

    Thread

---
+++ src/thread/ThreadManipulation/ThreadManipulation2.java
@@ -0,0 +1,21 @@
+package thread.ThreadManipulation;
+
+//thread manipulation using anonymous class (using runnable interface)
+public class ThreadManipulation2 {
+    public static void main(String[] args) {
+
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Running r1 ....");
+            }
+        };
+        Runnable r2 = () ->
+                System.out.println("Running r2....");  //using lambda , will discuss lambda later
+        Thread t1 = new Thread(r1);
+        Thread t2 = new Thread(r2);
+        t1.start();
+        t2.start();
+    }
+
+}
