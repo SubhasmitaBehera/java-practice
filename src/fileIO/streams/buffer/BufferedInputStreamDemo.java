d7d24d65e5dee4c788e81b34a6a227ff99f4d891
SubhasmitaBehera <beherasubhasmita1999@gmail.com>
2020-10-30 22:16:13 +0530

    FileIO

--- src/fileIO/streams/buffer/BufferedInputStreamDemo.java
+++ src/fileIO/streams/buffer/BufferedInputStreamDemo.java
@@ -1,29 +1,28 @@
-//package fileIO.streams.buffer;
-//
-//import java.io.BufferedInputStream;
-//import java.io.FileInputStream;
-//import java.io.IOException;
-//
-//public class BufferedInputStreamDemo {
-//    // read operation
-//    public static void main(String[] args) throws IOException {
-//        FileInputStream fis = new FileInputStream("file.txt");
-//        BufferedInputStream bis = new BufferedInputStream(fis);
-////        BufferedInputStream bis = new BufferedInputStream(new FileInputStream("file.txt"));
-//        int availableCapacity = bis.available();
-//        System.out.println(availableCapacity);
-//        for (int i = 0; i < availableCapacity; i++) {
-//            int j = bis.read();
-//            System.out.print((char)j);
-//        }
-////        System.out.println(bis.markSupported());
-////        bis.mark(11);
-////        bis.reset();
-////        for (int i = 0; i < 11; i++) {
-////            int j = bis.read();
-////            System.out.print((char)j);
-////        }
-//
-//
-//    }
-//}
+package fileIO.streams.buffer;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class BufferedInputStreamDemo {
+    // read operation
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream("file.txt");
+        BufferedInputStream bis = new BufferedInputStream(fis);
+//        BufferedInputStream bis = new BufferedInputStream(new FileInputStream("file.txt"));
+        int availableCapacity = bis.available();
+        System.out.println(availableCapacity);
+        for (int i = 0; i < availableCapacity; i++) {
+            int j = bis.read();
+            System.out.print((char)j);
+        }
+        System.out.println();
+        System.out.println(bis.markSupported());
+        bis.mark(11);
+        bis.reset();
+        for (int i = 0; i < 11; i++) {
+            int j = bis.read();
+            System.out.print((char)j);
+        }
+    }
+}
