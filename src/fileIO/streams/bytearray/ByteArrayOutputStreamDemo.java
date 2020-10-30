21d30f6e90102fa9719a42c3ce6ec657928a7201
SubhasmitaBehera <beherasubhasmita1999@gmail.com>
2020-10-30 22:20:34 +0530

    FileIO

--- src/fileIO/streams/bytearray/ByteArrayOutputStreamDemo.java
+++ src/fileIO/streams/bytearray/ByteArrayOutputStreamDemo.java
@@ -1,24 +1,24 @@
-//package fileIO.streams.bytearray;
-//
-//import java.io.ByteArrayOutputStream;
-//import java.io.FileOutputStream;
-//import java.io.IOException;
-//
-//public class ByteArrayOutputStreamDemo {
-//    // to write same type data to multiple files
-//    public static void main(String[] args) throws IOException {
-//        FileOutputStream fos1 = new FileOutputStream("file2.txt");
-//        FileOutputStream fos2 = new FileOutputStream("file3.txt");
-//        FileOutputStream fos3 = new FileOutputStream("file4.txt");
-//
-//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//        String s = "This is a string added to the file o/p stream";
-//        byte[] arr = s.getBytes();
-//
-//        baos.write(arr);
-//        baos.writeTo(fos1);
-//        baos.writeTo(fos2);
-//        baos.writeTo(fos3);
-//        System.out.println("success..");
-//    }
-//}
+package fileIO.streams.bytearray;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ByteArrayOutputStreamDemo {
+    // to write same type data to multiple files
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos1 = new FileOutputStream("file2.txt");
+        FileOutputStream fos2 = new FileOutputStream("file3.txt");
+        FileOutputStream fos3 = new FileOutputStream("file4.txt");
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        String s = "This is a string added to the file o/p stream";
+        byte[] arr = s.getBytes();
+
+        baos.write(arr);
+        baos.writeTo(fos1);
+        baos.writeTo(fos2);
+        baos.writeTo(fos3);
+        System.out.println("success..");
+    }
+}
