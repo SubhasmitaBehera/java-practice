dd8686173e538dd21ae0b051077593e3bb67bdc3
SubhasmitaBehera <beherasubhasmita1999@gmail.com>
2020-12-12 22:07:41 +0530

    Components

---
+++ src/GUI/AWT/ComponentsDemo.java
@@ -0,0 +1,29 @@
+package GUI.AWT;
+
+import java.awt.*;
+
+//public class ComponentsDemo extends Frame {
+//    ComponentsDemo(){
+//        Button btn = new Button("Button");
+//        btn.setBounds(100,200,80,80);
+//        add(btn);
+//        setSize(300,300); // size set for frame(container)
+//        setLayout(null);
+//        setVisible(true);
+//    }
+//    public static void main(String[] args) {
+//        ComponentsDemo cd = new ComponentsDemo();
+//    }
+//}
+
+public class ComponentsDemo{
+    public static void main(String[] args) {
+        Frame f = new Frame();
+        Button btn = new Button("Button");
+        btn.setBounds(100, 50, 80, 30);
+        f.add(btn);
+        f.setSize(300, 300); // size set for frame(container)
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+}
