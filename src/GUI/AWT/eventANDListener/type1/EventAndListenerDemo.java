d6e5035e9a8eac95a6996f4115cacff144cb528b
SubhasmitaBehera <beherasubhasmita1999@gmail.com>
2020-12-27 17:23:35 +0530

    AWT

---
+++ src/GUI/AWT/eventANDListener/type1/EventAndListenerDemo.java
@@ -0,0 +1,30 @@
+package GUI.AWT.eventANDListener.type1;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EventAndListenerDemo extends Frame implements ActionListener {
+    TextField tf ;
+    public EventAndListenerDemo() {
+        Button btn = new Button("Click ME");
+        btn.setBounds(100,60,80,40);
+        tf = new TextField();
+        tf.setBounds(100,150,100,30);
+        add(btn);
+        add(tf);
+        setSize(300,300);
+        setLayout(null);
+        setVisible(true);
+        btn.addActionListener(this);
+    }
+
+    public static void main(String[] args) {
+        new EventAndListenerDemo();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        tf.setText("this is a text field");
+    }
+}
